f152fd21a9f7b705d3245bba44d8c4095b319555
ataurwd <ataurrahman24707@gmail.com>
2025-03-17 15:51:01 +0600

    update server side  comment

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", User);
 
-// Server Start
+// Server Start ataurwd
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
