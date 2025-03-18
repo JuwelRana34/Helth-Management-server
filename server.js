a62ce2b0cb16abc55355642caddc77ed641c2a4e
Md. Al-Amin Islam Rahat <143124679+alaminislamrahat@users.noreply.github.com>
2025-03-18 15:23:33 +0600

    backend update (#2)

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 // const cors = require("cors");
 const connectDB  = require("./src/config/db");
 const authRoutes = require("./src/routes/auth.routes");
 const User = require("./src/routes/data.routes");
+const Post = require("./src/routes/post.routes");
+
 
 const app = express();
 connectDB ();
 // Middleware
 // app.use(cors());
+app.use(cors())
 app.use(express.json());
+
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api", User);
-
+app.use("/api", Post)
 // Server Start ataurwd
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    img : {type : String},
     role: { type: String, default: "patient" },
+    
   },
   { timestamps: true }
 );
