8fb5e74df44b127a1b35b7ac636ccb5cdde63103
Md. Al-Amin Islam Rahat <143124679+alaminislamrahat@users.noreply.github.com>
2025-03-20 17:10:36 +0600

    add doctor server created
    * backend update
    
    * add doctor api worke

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB  = require("./src/config/db");
 const authRoutes = require("./src/routes/auth.routes");
 const User = require("./src/routes/data.routes");
 const Post = require("./src/routes/post.routes");
+const Doctor = require("./src/routes/doctors.routes")
 
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api", User);
 app.use("/api", Post)
+app.use("/api", Doctor)
 // Server Start ataurwd
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
