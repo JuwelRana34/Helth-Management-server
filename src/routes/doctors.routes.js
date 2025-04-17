e7d5f42211bf515098ee8f69bdcc2a649f928b4f
alaminislamrahat <alaminislamrahat@gmail.com>
2025-04-17 22:06:07 +0600

    single doctor added

--- src/routes/doctors.routes.js
+++ src/routes/doctors.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-const {addDoctor, getDoctor, deleteDoctor} = require('../controllers/doctors.controller')
+const { addDoctor, getDoctor, deleteDoctor,singleDoctor } = require('../controllers/doctors.controller')
 
 
 const router = express.Router();
 
-router.post('/doctor',addDoctor)
-router.delete('/doctor/:id',deleteDoctor)
-router.get('/doctor',getDoctor)
+router.post('/doctor', addDoctor)
+router.delete('/doctor/:id', deleteDoctor)
+router.get('/doctor', getDoctor)
+router.get('/doctor/:id',singleDoctor)
 
 
 module.exports = router;
