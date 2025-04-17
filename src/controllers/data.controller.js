d1110e7eafa36d1fb4545b4830df02726da3d3b5
JuwelRana34 <juwelrana3426@gmail.com>
2025-04-17 23:53:11 +0600

    Implement user role management and payment retrieval features; add update and delete user endpoints

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -18,3 +18,46 @@ exports.spcificUser = async function (req, res) {
     res.status(500).json({ message: "Error fetching data from database", error });
   }
 }
+exports.updateUser = async function (req, res) {
+  const userId = req.params.id;
+  const { role } = req.body;
+
+  try {
+    if (!role) {
+      return res.status(400).json({ message: "Role is required" });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { role },
+      { new: true, runValidators: true }
+    );
+    
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user: updatedUser });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res.status(500).json({ message: "Error updating user", error: error.message });
+  }
+};
+exports.DeleteUser = async function (req, res) {
+  const userId = req.params.id;
+  try {
+    if (!userId) {
+      return res.status(400).json({ message: "Role is required" });
+    }
+
+  await User.findByIdAndDelete(userId);
+
+    res.status(200).json({ user: "user successfully deleted" });
+  } catch (error) {
+    console.error("Error updating user:", error);
+    res
+      .status(500)
+      .json({ message: "Error updating user", error: error.message });
+  }
+
+};
