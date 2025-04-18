1460cdad3ef3bad90fbad75b6a481423ba76cff2
JuwelRana34 <juwelrana3426@gmail.com>
2025-04-18 12:17:43 +0600

    Add all details retrieval and booked schedules endpoints

--- src/routes/schedule.routes.js
+++ src/routes/schedule.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getBookedSchedule, bookSchedule, getScheduleByDoctorAndDate} = require("../controllers/schedule.controller");
+const { getBookedSchedule, bookSchedule, getScheduleByDoctorAndDate, getALLBookedSchedule} = require("../controllers/schedule.controller");
 
 router.get("/schedule/:userId",getBookedSchedule)
 router.post("/bookSchedule",bookSchedule )
 router.get("/schedule",getScheduleByDoctorAndDate )
+router.get("/AllSchedule",getALLBookedSchedule )
 
 module.exports = router;
