d5810a4dc4d49faf4760d123979dbc7c25044f20
JuwelRana34 <juwelrana3426@gmail.com>
2025-04-22 00:13:59 +0600

    Add data aggregation in AllDetails endpoint and fix typo in routes

--- src/controllers/data.controller.js
+++ src/controllers/data.controller.js
@@ -1,4 +1,8 @@
 const User = require("../models/user.model");
+const Payment = require("../models/payments.model");
+const Schedule = require("../models/Schedule.model");
+
+
 exports.User = async function (req, res) {
   try {
     const users = await User.find()
@@ -61,8 +65,45 @@ exports.DeleteUser = async function (req, res) {
   }
 
 };
-exports.AllDetais = async function (req, res) {
- 
-  // const allDetails = Ueser.aggrigate
+exports.AllDetails = async function (req, res) {
+  try {
+    // Aggregating Users Data
+    const userStats = await User.aggregate([
+      {
+        $group: {
+          _id: "$role",
+          count: { $sum: 1 }
+        }
+      }
+    ]);
+    
+    const roleCounts = {};
+    userStats.forEach(stat => {
+      roleCounts[stat._id] = stat.count;
+    });
 
+    // Aggregating Payments Data
+    const paymentStats = await Payment.aggregate([
+      {
+        $group: {
+          _id: null,
+          totalAmount: { $sum: "$amount" }
+        }
+      }
+    ]);
+
+    // Aggregating Schedules Data
+    const totalSchedules = await Schedule.countDocuments();
+
+    // Send all data back as a response
+    res.status(200).json({
+      userStats: roleCounts,
+      totalPayments: paymentStats[0]?.totalAmount || 0,
+      totalSchedules
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching data from database", error });
+  }
 };
+
