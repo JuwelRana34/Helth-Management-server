c7f595ac9d716d29de890b2756b7ae669253e88a
Md. Al-Amin Islam Rahat <143124679+alaminislamrahat@users.noreply.github.com>
2025-04-25 15:19:08 +0600

    Rahat (#41)
    * backend update
    
    * add doctor api worke
    
    * get route added on doctor
    
    * done
    
    * jwt added
    
    * single doctor added
    
    * booking
    
    * be a doctor created

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {DeleteUser, User, spcificUser,updateUser ,AllDetails} = require("../controllers/data.controller");
+const {DeleteUser, User, spcificUser,updateUser ,AllDetails,UserStatus,RemoveStatus} = require("../controllers/data.controller");
 
 const router = express.Router();
 
 router.get("/users", User);
 router.get("/user/:email", spcificUser);
 router.patch("/users/role/:id", updateUser);
+router.patch("/users/status/:email", UserStatus);
+router.patch("/users/remove-status/:email", RemoveStatus);
 router.delete("/users/:id", DeleteUser);
 router.get("/allDetailsAboutWebiste", AllDetails);
 
